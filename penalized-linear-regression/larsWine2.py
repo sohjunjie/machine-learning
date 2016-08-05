360ed0d1c1293778f1dd0271f434be821a252971
sohjunjie <junjie.soh93@gmail.com>
2016-08-05 17:27:25 +0800

    track only folders

--- penalized-linear-regression/larsWine2.py
+++ penalized-linear-regression/larsWine2.py
@@ -4,6 +4,24 @@ from sklearn import datasets, linear_model
 from math import sqrt
 import matplotlib.pyplot as plot
 
+"""
+LARS Algorithm
+
+The algorithm attempts to solve the minimization problem by incrementing the beta
+for whichever columns can reduce the algorithm error the most.
+
+STEPS
+1. Initialize step size, step to loop, beta of 0 for all columns
+2. Estimate the label for each row data by using xNormalized x beta
+3. Residual is the error between estimated label and actual label
+4. Estimate the correlation between each columns vs residual
+5. Column with highest correlation will have its beta increment
+6. The residual will decrease in subsequent loop and the previous column will be
+   less effective influencing the residual
+7. Other column beta will eventually be incremented, solving the minimization
+   problem
+"""
+
 xList = []
 labels = []
 names = []
@@ -19,3 +37,78 @@ with open('winequality-red.csv','r') as myFile:
             row.pop()
             floatRow = [float(num) for num in row]
             xList.append(floatRow)
+
+#Normalize columns in x and labels
+nrows = len(xList)
+ncols = len(xList[0])
+
+#calculate means and variances
+xMeans = []
+xSD = []
+for i in range(ncols):
+    col = [xList[j][i] for j in range(nrows)]
+    mean = sum(col)/nrows
+    xMeans.append(mean)
+    colDiff = [(xList[j][i] - mean) for j in range(nrows)]
+    sumSq = sum([colDiff[i] * colDiff[i] for i in range(nrows)])
+    stdDev = sqrt(sumSq/nrows)
+    xSD.append(stdDev)
+
+#use calculate mean and standard deviation to normalize xList
+xNormalized = []
+for i in range(nrows):
+    rowNormalized = [(xList[i][j] - xMeans[j])/xSD[j] for j in range(ncols)]
+    xNormalized.append(rowNormalized)
+
+#Normalize labels
+meanLabel = sum(labels)/nrows
+sdLabel = sqrt(sum([(labels[i] - meanLabel) * (labels[i] - meanLabel) for i in range(nrows)])/nrows)
+
+labelNormalized = [(labels[i] - meanLabel)/sdLabel for i in range(nrows)]
+
+#initialize a vector of coefficients beta
+beta = [0.0] * ncols
+
+#initialize matrix of betas at each step
+betaMat = []
+betaMat.append(list(beta))
+
+#number of steps to take
+nSteps = 350
+stepSize = 0.004
+
+for i in range(nSteps):
+    #calculate residuals
+    residuals = [0.0] * nrows
+    for j in range(nrows):
+        labelsHat = sum([xNormalized[j][k] * beta[k] for k in range(ncols)])
+        residuals[j] = labelNormalized[j] - labelsHat
+
+    #calculate correlation between attribute columns from
+    #normalized wine and residual
+    corr = [0.0] * ncols
+
+    for j in range(ncols):
+        corr[j] = sum([xNormalized[k][j] * residuals[k] for k in range(nrows)]) / nrows
+
+    iStar = 0
+    corrStar = corr[0]
+
+    for j in range(1, (ncols)):
+        if abs(corrStar) < abs(corr[j]):
+            iStar = j; corrStar = corr[j]
+
+    beta[iStar] += stepSize * corrStar / abs(corrStar)
+    betaMat.append(list(beta))
+
+
+for i in range(ncols):
+    #plot range of beta values for each attribute
+    coefCurve = [betaMat[k][i] for k in range(nSteps)]
+    # print(max(coefCurve))
+    xaxis = range(nSteps)
+    plot.plot(xaxis, coefCurve)
+
+plot.xlabel("Steps Taken")
+plot.ylabel(("Coefficient Values"))
+plot.show()
